eb3fe6dfc6460837b62620d4f8e094897a95b805
zzptichka <shkvorets@gmail.com>
2018-10-31 10:10:06 -0400

    getgreen.sh script to overpass greenspace and convert to json

--- getgreen.sh
+++ getgreen.sh
@@ -1,1 +1,3 @@
-wget -nv -O "data/greenspace.json" --post-file="greenspace.query" "http://overpass-api.de/api/interpreter"
+wget -nv -O "data/greenspace.osm" --post-file="greenspace.query" "http://overpass-api.de/api/interpreter"
+
+osmtogeojson data/greenspace.osm > data/greenspace.json
